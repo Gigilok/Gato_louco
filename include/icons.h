3c6b2e125ca84e58fd4ab9ed61174724434a60e8
Gigilok <cliquefeirago@gmail.com>
2026-07-19 16:38:45 -0300

    Add files via upload

---
+++ include/icons.h
@@ -0,0 +1,12 @@
+#ifndef ICONS_H
+#define ICONS_H
+
+#include <Arduino.h>
+
+// Apenas gatos para boot animation
+extern const unsigned char cat_open[] PROGMEM;
+extern const unsigned char cat_blink[] PROGMEM;
+
+// Menu icons removidos da tela OLED - apenas texto
+
+#endif
